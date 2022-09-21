0392de45c30c586889a97f12972ff093add83596
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 10:51:31 +0900

    devise-token-auth初期設定

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,5 +8,18 @@ module Backend
   class Application < Rails::Application
     config.load_defaults 7.0
     config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:4000'
+        resource '*',
+                 :headers => :any,
+                 :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                 :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end

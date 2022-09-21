fd2f7d7b9c4b08e0bcb2d68a0d228ae99b008eec
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 14:15:53 +0900

    contextの設定

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LayOut } from './lib/components/LayOut';
 import { SignUp } from './lib/components/SignUp';
+import { getCurrentUser } from "./lib/api/session.js"
+
+export const AuthContext = React.createContext("");
 
 export const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<LayOut/>}>
-         <Route path={"/signup"} element={<SignUp/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={isSignedIn}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<LayOut/>}>
+          <Route path={"/signup"} element={<SignUp/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };

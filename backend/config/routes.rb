34042d74c34fc93be1307e3e64e37a1b4691598f
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 11:28:06 +0900

    ルーティング設定を修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
+    registrations: 'api/v1/auth/registrations'
   }
-  namespace :auth do
-    resources :sessions, only: %i[index]
+  namespace :api do
+    namespace :v1 do
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
   end
 end

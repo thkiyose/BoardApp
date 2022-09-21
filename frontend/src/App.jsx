34ca73668395496fdca01005c8781df1f70ddd85
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 14:50:41 +0900

    ユーザーロード状態管理用のstateを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,23 +7,25 @@ import { getCurrentUser } from "./lib/api/session.js"
 export const AuthContext = React.createContext("");
 
 export const App = () => {
+  const [loading, setLoading] = useState(true)
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
 
   const handleGetCurrentUser = async () => {
     try {
-      const res = await getCurrentUser()
+      const res = await getCurrentUser();
 
       if (res?.data.isLogin === true) {
-        setIsSignedIn(true)
-        setCurrentUser(res?.data.data)
-        console.log(res?.data.data)
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.data);
+        console.log(res?.data.data);
       } else {
-        console.log("No current user")
+        console.log("No current user");
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
+    setLoading(false);
   }
 
   useEffect(() => {

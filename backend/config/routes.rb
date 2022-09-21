72d1ae307724ed8bfff672736279fdd4b3019587
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 11:17:27 +0900

    ルーティング設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
 end

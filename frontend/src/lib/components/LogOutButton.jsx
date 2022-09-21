37e3fc9cf5c6072f3ed5956cf20ac06434d1b273
haruka tezuka <th_kiyose@outlook.com>
2022-09-21 15:00:49 +0900

    ログアウト用コンポーネント作成

---
+++ frontend/src/lib/components/LogOutButton.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import Cookies from "js-cookie"
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "App"
+
+export const LogOutButton = () => {
+    return (
+        <p>test</p>
+    )
+}

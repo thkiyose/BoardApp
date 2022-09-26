5a182b61634ce5ad1677845e6cca9f2c23f0d6df
haruka tezuka <th_kiyose@outlook.com>
2022-09-26 11:32:46 +0900

    セクションのseedデータを投入

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Section.create(sections: 0, areas: 0 )
+Section.create(sections: 0, areas: 1 )
+Section.create(sections: 0, areas: 2 )
+Section.create(sections: 1, areas: 1 )
+Section.create(sections: 2, areas: 0 )
+Section.create(sections: 2, areas: 2 )
+Section.create(sections: 3, areas: 0 )
+Section.create(sections: 3, areas: 1 )
+Section.create(sections: 3, areas: 2 )
+Section.create(sections: 4, areas: 2 )

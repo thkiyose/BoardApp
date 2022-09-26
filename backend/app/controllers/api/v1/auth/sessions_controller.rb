1b6f9c58e8b265f83d4ddaa5b81596cd6dbc5817
haruka tezuka <th_kiyose@outlook.com>
2022-09-26 15:28:13 +0900

    ユーザーの所属セクションの表示を作成

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Auth::SessionsController < ApplicationController
     def index
-        if current_user
-          render json: { is_login: true, data: current_user }
-        else
-          render json: { is_login: false, message: "ユーザーが存在しません" }
-        end
+      if current_user
+        render json: { is_login: true, data: { user: current_user, sections: current_user.sections.group_by { |s| s.sections }} }
+      else
+        render json: { is_login: false, message: "ユーザーが存在しません" }
       end
+    end
 end

63187d26e6cdbe2a4877a2f804a911e9ca6a62c1
haruka tezuka <th_kiyose@outlook.com>
2022-09-27 15:44:53 +0900

    リレーション設定

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
   validates :name, length: { maximum: 30 }, presence: true
   has_many :user_sections, dependent: :destroy
   has_many :sections, through: :user_sections
+  has_many :news
 end

42c4bf6834915f00d9cd055b742953c5432a657b
haruka tezuka <th_kiyose@outlook.com>
2022-09-27 10:24:48 +0900

    sections/createアクションを定義、ルーティング設定

--- backend/app/controllers/api/v1/sections_controller.rb
+++ backend/app/controllers/api/v1/sections_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::SectionsController < ApplicationController
     def index
         render json: { sections: Section.all.group_by { |s| s.sections }} 
     end
+
+    def create
+        user = User.find_by(id: params[:id])
+        selected = params[:selected]
+        selected.each do |x|
+         put x
+        end
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       namespace :auth do
         resources :sessions, only: %i[index]
       end
-      resources :sections, only: %i[index]
+      resources :sections, only: %i[index, create]
     end
   end
 end

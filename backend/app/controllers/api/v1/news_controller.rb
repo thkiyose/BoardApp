5000fa94b320c4bd4d06b5f696a5cd6e055c4fc0
haruka tezuka <th_kiyose@outlook.com>
2022-09-27 15:48:15 +0900

    news/createアクション定義

--- backend/app/controllers/api/v1/news_controller.rb
+++ backend/app/controllers/api/v1/news_controller.rb
@@ -3,5 +3,18 @@ class Api::V1::NewsController < ApplicationController
   end
 
   def create
+    news = News.new(news_params)
+    if news.save
+      render json: { status:"success" }
+    else
+      render json { errors: news.errors}
+    end
+    
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:title,:content,:user_id)
   end
 end

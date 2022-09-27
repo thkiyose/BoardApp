003e4924055d23eb920b9bddb397bfb3fe63b03d
haruka tezuka <th_kiyose@outlook.com>
2022-09-27 16:46:19 +0900

    新規投稿画面へのリンク追加

--- frontend/src/lib/components/News.jsx
+++ frontend/src/lib/components/News.jsx
@@ -1,12 +1,49 @@
 import React, { useState } from 'react';
-import { useLocation } from "react-router-dom"
+import styled from 'styled-components';
+import { useLocation, useNavigate, Link } from "react-router-dom"
 import { FlashMessage } from './common/FlashMessage';
+import Color from '../components/common/Color';
 
 export const News = (props) => {
     const location = useLocation();
     const [ message ] = useState(location.state ?  [location.state.message] : []);
+    const navigate = useNavigate();
     
-    return (<>
+    return (<Div>
         <FlashMessage message={message} type={"success"} />
-    </>)
+        <CreateButton/>
+    </Div>)
 }
+
+const CreateButton = () => {
+    
+    return (
+        <CreateLink to="create">
+            新規投稿
+        </CreateLink>
+    )
+}
+
+const Div = styled.div`
+    margin-top: 10px;
+`
+
+const CreateLink = styled(Link)`
+    border-radius: 50%;
+    line-height: 70px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    cursor: pointer;
+    width: 70px;
+    height: 70px;
+    padding: 0;
+    border: none;
+    background: ${Color.primary};
+    color: white;
+    :hover {
+        -webkit-transform: translate(0, 3px);
+        transform: translate(0, 3px);
+
+      }
+`

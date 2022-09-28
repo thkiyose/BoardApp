4c9ca7a74547a6529e7a154eed3c99c1b14633aa
haruka tezuka <th_kiyose@outlook.com>
2022-09-28 11:01:04 +0900

    アソシエーション設定

--- backend/app/models/news.rb
+++ backend/app/models/news.rb
@@ -3,4 +3,8 @@ class News < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30}
   validates :content, presence: true
+  has_many :news_to_sections, dependent: :destroy
+  has_many :news_from_sections, dependent: :destroy
+  has_many :to_sections, through: :news_to_sections, source: :section
+  has_many :from_sections, through: :news_from_sections, source: :section
 end

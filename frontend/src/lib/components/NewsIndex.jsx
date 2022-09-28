90ceb4b3245a20c3e727b0eb33aefa1cb9e084b7
haruka tezuka <th_kiyose@outlook.com>
2022-09-28 14:24:49 +0900

    ニュースデータの取得処理を設定

--- frontend/src/lib/components/NewsIndex.jsx
+++ frontend/src/lib/components/NewsIndex.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
+import { IndexNews } from '../api/news';
 
 export const NewsIndex = () => {
+    const [ news, setNews ] = useState([]);
+
+    const LoadNews = async() => {
+        try {
+            const res = await IndexNews(); 
+            if (res.status === 200) {
+                setNews(res.data.news);
+            } else {
+              console.log(res)
+            }
+          } catch (e) {
+            console.log(e)
+          }
+    }
+    useEffect(()=>{LoadNews()},[setNews]);
+
     return (
         <Div>
+            {news.map((n)=>{
+                return (
+                    <p>{n.title}</p>
+                )
+            })}
         </Div>
     )
 }

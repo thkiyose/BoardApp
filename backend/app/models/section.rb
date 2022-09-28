2f08e609d8014f014ee190fc7ee5907aef230d39
haruka tezuka <th_kiyose@outlook.com>
2022-09-28 14:17:59 +0900

    axios設定

--- backend/app/models/section.rb
+++ backend/app/models/section.rb
@@ -3,4 +3,6 @@ class Section < ApplicationRecord
     enum :areas, { A: 0, B: 1, C: 2 }
     has_many :user_sections, dependent: :destroy
     has_many :users, through: :user_sections
+    has_many :news_to_sections, dependent: :destroy
+    has_many :news_from_sections, dependent: :destroy
 end
--- frontend/src/lib/api/news.js
+++ frontend/src/lib/api/news.js
@@ -1,5 +1,9 @@
 import { client } from './client';
 
+export const IndexNews = () => {
+    return client.get('/news');
+};
+
 export const createNews = (params) => {
     return client.post('/news', params);
 };

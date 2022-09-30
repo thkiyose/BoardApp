8878b585c8610d2ec89ee3e0c558373ee34700ff
haruka tezuka <th_kiyose@outlook.com>
2022-09-30 09:58:28 +0900

    タブの作成

--- frontend/src/lib/components/NewsIndex.jsx
+++ frontend/src/lib/components/NewsIndex.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Color from './common/Color';
 import styled from 'styled-components'
 import { IndexNews } from '../api/news';
 import { NewsCard } from './common/NewsCard';
@@ -21,20 +22,61 @@ export const NewsIndex = () => {
     useEffect(()=>{LoadNews()},[setNews]);
 
     return (
-        <Div>
-            {news.map((n)=>{
-                return (
-                    <NewsCard news={n}/>
-                )
-            })}
-        </Div>
+        <>
+            <Tab/>
+            <Div>
+                {news.map((n,index)=>{
+                    return (
+                        <NewsCard key={index} news={n}/>
+                    )
+                })}
+            </Div>
+        </>
     )
 }
 
 const Div = styled.div`
-    padding: 0;
+    padding: 20px 0px;
     display: flex;
     flex-wrap : wrap;
     width: 80%;
+    background: ${Color.secondary};
     justify-content: stretch; 
+`
+
+const Tab = () => {
+
+    return (
+        <TabDiv className="tabs">
+            <label className="tab_item">全て</label>
+            <label className="tab_item">自分の所属</label>
+            <label className="tab_item">検索</label>
+        </TabDiv>
+    )
+}
+
+const TabDiv = styled.div`
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    margin: 0 auto;
+  .tab_item {
+    width: 10%;
+    height: 40px;
+    margin-top: 10px;
+    margin-right: 10px;
+    background-color: gray;
+    line-height: 40px;
+    text-align: center;
+    color: #fff;
+    font-size: 0.9rem;
+    display: block;
+    float: left;
+    text-align: center;
+    transition: all 0.2s ease;
+  }
+  .tab_item:hover {
+    opacity: 0.75;
+  }
+  input[name="tab_item"] {
+    display: none;
+  }
 `

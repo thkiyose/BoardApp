c292f836b534b75973463a3e33f5bbdee23df9bf
haruka tezuka <th_kiyose@outlook.com>
2022-09-30 10:56:32 +0900

    News検索用スコープを定義

--- backend/app/models/news.rb
+++ backend/app/models/news.rb
@@ -9,4 +9,32 @@ class News < ApplicationRecord
   has_many :from_sections, through: :news_from_sections, source: :section
   accepts_nested_attributes_for :news_to_sections, allow_destroy: true
   accepts_nested_attributes_for :news_from_sections, allow_destroy: true
+
+  scope :search_with_section_id, -> (id,type){
+    return if id.blank? || type.blank?
+    if type === "to"
+      joins(:to_sections).where(sections:{id: id})
+    elsif type === "from"
+      joins(:from_sections).where(sections:{id: id})
+    end
+  }
+
+  scope :search_with_section_only, -> (section,type){
+    return if section.blank? || type.blank?
+    if type === "to"
+      joins(:to_sections).where(sections:{sections: section})
+    elsif type === "from"
+      joins(:from_sections).where(sections:{sections: section})
+    end
+  }
+
+
+  scope :search_with_area_only, -> (section,type){
+    return if section.blank? || type.blank?
+    if type === "to"
+      joins(:to_sections).where(sections:{areas: area})
+    elsif type === "from"
+      joins(:from_sections).where(sections:{areas: area})
+    end
+  }
 end

fddfabff193dc18fbe26d03fc0694c288cbafb7e
haruka tezuka <th_kiyose@outlook.com>
2022-10-03 10:59:42 +0900

    コアスタイルの設定

--- frontend/src/lib/components/Schedule.jsx
+++ frontend/src/lib/components/Schedule.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import 'moment/locale/ja';
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment)
 

efc0148374e2ae132b6989c0baea7ba30ece1922
haruka tezuka <th_kiyose@outlook.com>
2022-10-03 16:56:52 +0900

    イベントフォームの作成

--- frontend/src/lib/components/Schedule.jsx
+++ frontend/src/lib/components/Schedule.jsx
@@ -5,6 +5,7 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import 'moment/locale/ja';
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import Modal from "react-modal";
 import { CreateEvent } from '../api/event'
 import { FetchEvents } from '../api/event'
 
@@ -12,6 +13,8 @@ const localizer = momentLocalizer(moment)
 
 export const Schedule = () => {
     const [events, setEvents] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [params, setParams] = useState({});
 
     const loadEvents = async() => {
         try {
@@ -37,27 +40,26 @@ export const Schedule = () => {
 
     const handleSelectSlot = useCallback(
         async({ start, end }) => {
-          const title = window.prompt('New Event Name')
-          if (title) {
-            const res = await CreateEvent({title: title, start: start, end: end}) 
-            if (res.status === 200) {
-                setEvents((prev) => [...prev, { start, end, title }])
-            } else {
-                console.log(res)
-            }
-          }
+          setShowModal(true);
+        //   if (title) {
+        //     const res = await CreateEvent({title: title, start: start, end: end}) 
+        //     if (res.status === 200) {
+        //         setEvents((prev) => [...prev, { start, end, title }])
+        //     } else {
+        //         console.log(res)
+        //     }
+        //   }
         },
         [setEvents]
       )
 
-console.log(formatted)
       const handleSelectEvent = useCallback(
         (event) => window.alert(event.title),
         []
       )
 
     return (
-        <Div>
+        <Div id="calendar">
             <button>イベント追加</button>
             <Calendar
             localizer={localizer}
@@ -69,6 +71,7 @@ console.log(formatted)
             endAccessor="end"
             style={{ height: 600 }}
             />
+            <ScheduleModal showModal={showModal} setShowModal={setShowModal}/>
       </Div>
     )
 }
@@ -80,6 +83,9 @@ const Div = styled.div`
     .rbc-toolbar {
         background: ${Color.secondary};
         margin: 0;
+    }
+    .rbc-row-content {
+        z-index: 0;
     }
     .rbc-header {
         background: ${Color.primary};
@@ -135,5 +141,81 @@ const Div = styled.div`
         font-size: 14px;
         content: 'スケジュール';
     }
+    .rbc-btn-group:nth-child(3) > button:nth-child(4)::before{
+        font-size: 14px;
+        content: 'スケジュール';
+    }
+}
+`
 
+const ScheduleModal = (props) => {
+    const { showModal, setShowModal } = props;
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          minWidth: '80%',
+          background: `${Color.bg}`,
+          transform: 'translate(-50%, -50%)'
+        },
+        overlay: {
+            background: 'rgb(0,0,0,0.5)'
+        }
+      };
+  
+    const closeModal = () => {
+      setShowModal(false);
+    }
+
+    return (
+        <Modal
+            isOpen={showModal}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            style={customStyles}
+            areaHideApp={false}
+        >
+            <InsideMordal>
+                <h2>イベント作成</h2>
+                <form>
+                <label>タイトル</label>
+                <input />
+                <label>開始</label>
+                <input />
+                <label>終了</label>
+                <input />
+                <input type="checkBox" name="allDay" />
+                <span>終日</span>
+                </form>
+            </InsideMordal>
+        </Modal>
+    );
+}
+
+const InsideMordal = styled.div`
+    font-size: 0.8rem;
+    h2 {
+        text-align: center;
+    }
+    label {
+        display: block;
+        margin-left: 10%;
+        margin-top: 20px;
+    }
+    input {
+        margin-left: 10%;
+        width: 80%;
+        background: none;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: solid 1px gray;
+        padding: 8px;
+    }
+    input[type="checkBox"] {
+        width: 20px;
+        margin-top: 20px;
+        margin-bottom: 30px;
+    }
 `

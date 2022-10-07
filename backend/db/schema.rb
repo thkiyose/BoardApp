3755de3ce3120dd32e09810d8206feb9f68a7371
haruka tezuka <th_kiyose@outlook.com>
2022-10-07 10:15:54 +0900

    footprintsのカラムの組み合わせにユニーク制約を追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_010619) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_011422) do
   create_table "event_sections", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.bigint "section_id", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_010619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["news_id"], name: "index_footprints_on_news_id"
+    t.index ["user_id", "news_id"], name: "index_footprints_on_user_id_and_news_id", unique: true
     t.index ["user_id"], name: "index_footprints_on_user_id"
   end
 

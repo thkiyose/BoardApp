3509c53340dbfb41d0ece1a9aa931c112d1d44a6
haruka tezuka <th_kiyose@outlook.com>
2022-10-12 15:29:11 +0900

    通知データ送信用のアクションを定義

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'notifications/index'
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
     registrations: 'api/v1/auth/registrations'
   }

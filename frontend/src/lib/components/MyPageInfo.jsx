14e272cdd8c759a561134aba530c217de6a62920
haruka tezuka <th_kiyose@outlook.com>
2022-10-13 13:50:17 +0900

    マイページの所属表示を改善

--- frontend/src/lib/components/MyPageInfo.jsx
+++ frontend/src/lib/components/MyPageInfo.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../App.jsx';
 import styled from 'styled-components'
+import Color from './common/Color.jsx';
 
 export const MyPageInfo = () => {
     const { currentUser } = useContext(AuthContext)
@@ -14,13 +15,16 @@ export const MyPageInfo = () => {
                 <li>{currentUser.user.email}</li>
                 <label>ユーザー種別</label>
                 <li>{currentUser.user.admin ? "管理者": "一般"}</li>
-                <label>所属セクション</label>
-                    {Object.keys(currentUser.sections).map((key,value) => {
-                    const tags = [<li className="sectionName">{key}:</li>]
-                    currentUser.sections[key].forEach((sec)=> tags.push(<span className="areaName">{sec.areas}</span>))
-                    tags.push(<br/>)
-                    return <div>{tags}</div>
-                })}
+                <label className="secLabel">所属セクション</label>
+                {Object.keys(currentUser.sections).map((key)=>{
+                    const children = []
+                    currentUser.sections[key].forEach((sec,index)=>{
+                       children.push(<span key={index} className="area">{sec.areas}</span>);
+                    })
+                    const li = React.createElement("li", {key: key, className:"secArea"}, <SectionArea>{key}:{[...children]}</SectionArea>)
+                    return li;
+                })
+                }         
             </ul>
         </InfoDiv>
     )
@@ -29,15 +33,25 @@ export const MyPageInfo = () => {
 const InfoDiv = styled.div`
     ul {
         list-style: none;
-        div {
-            margin: 5px;
-        }
     }
-    .sectionName {
-        display: inline;
-        margin-right: 5px;
+    .secLabel {
+        display: block;
+        margin-bottom: 5px;
     }
-    .areaName {
-        margin: 0px 3px;
+    .secArea {
+        margin: 10px 0px;
+    }
+`
+const SectionArea = styled.span`
+    background: ${Color.secondary};
+    color: white;
+    padding: 5px;
+    margin-left: 5px;
+    border-radius: 5px;
+    .area {
+        margin-left: 3px;
+        background: ${Color.primary};
+        padding: 5px;
+        border-radius: 10px;
     }
 `
